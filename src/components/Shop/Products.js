0aca8cfd84086dc6b141fada63e97ee6ad2ea9f7
smileyismail <79481480+smileyismail@users.noreply.github.com>
2022-09-17 16:44:46 +0530

    sort by category added

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -2,17 +2,17 @@ import { useEffect, useState, useCallback } from "react";
 import ProductItem from "./ProductItem";
 import classes from "./Products.module.css";
 
-let productsData = [];
-
 const Products = (props) => {
+  const [products, setProducts] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [searchTerm, setSearchTerm] = useState("");
+  const [category, setCategory] = useState("");
 
   const getProducts = useCallback(async () => {
     setIsLoading(true);
     const response = await fetch("https://fakestoreapi.com/products");
     const data = await response.json();
-    productsData = data;
+    setProducts(data);
     setIsLoading(false);
   }, []);
 
@@ -20,26 +20,63 @@ const Products = (props) => {
     getProducts();
   }, [getProducts]);
 
+  function selectCategoryHandler(e) {
+    setCategory(e.target.value);
+    setSearchTerm("");
+  }
+
+  let sortedProducts = products.filter((product) => {
+    if (category === "men's clothing") {
+      return product.category === "men's clothing";
+    }
+    if (category === "jewelery") {
+      return product.category === "jewelery";
+    }
+    if (category === "electronics") {
+      return product.category === "electronics";
+    }
+    if (category === "women's clothing") {
+      return product.category === "women's clothing";
+    }
+
+    return product;
+  });
+
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <div className={classes.inputs}>
-        <div></div>
+        <div className={classes.sort}>
+          <label htmlFor="category">Select Category: </label>
+          <select
+            id="category"
+            value={category}
+            onChange={selectCategoryHandler}
+          >
+            <option value="all">All</option>
+            <option value="men's clothing">Men's clothing</option>
+            <option value="jewelery">Jewelry</option>
+            <option value="electronics">Electronics</option>
+            <option value="women's clothing">Women's clothing</option>
+          </select>
+        </div>
         <div>
           <label htmlFor="search">Search: </label>
           <input
             type="text"
             id="search"
+            value={searchTerm}
             placeholder="Search Product..."
             onChange={(e) => {
               setSearchTerm(e.target.value);
+              setCategory("all");
             }}
           />
         </div>
       </div>
       {!isLoading && (
         <ul>
-          {productsData
+          {sortedProducts
             .filter((product) =>
               product.title.toLowerCase().includes(searchTerm.toLowerCase())
             )
@@ -48,6 +85,7 @@ const Products = (props) => {
                 key={product.id}
                 id={product.id}
                 title={product.title}
+                a
                 price={product.price}
                 description={product.description}
                 image={product.image}
